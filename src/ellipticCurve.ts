7538b13b8159c29d42a36ec05422cc3fe8d205d6
pillip <pillip.youn@gmail.com>
2024-01-31 17:40:05 +0900

    Refactor: delete unnecessary line

--- src/ellipticCurve.ts
+++ src/ellipticCurve.ts
@@ -1,125 +1,139 @@
 import { FieldElement, PrimeGaloisField } from "./galoisFields";
 
 interface EllipticCurveInterface {
-    a: FieldElement;
-    b: FieldElement;
-    field : PrimeGaloisField;
-    isContained(point: Point): boolean;
+  a: FieldElement;
+  b: FieldElement;
+  field: PrimeGaloisField;
+  isContained(point: Point): boolean;
 }
 
 interface PointInterface {
-    x: FieldElement;
-    y: FieldElement;
-    curve: EllipticCurve;
-    isIdentity(): boolean;
-    add(other: Point): Point;
-    scalarMul(scalar: number): Point;
-};
-
-class EllipticCurve implements EllipticCurveInterface {
-    a: FieldElement;
-    b: FieldElement;
-    field : PrimeGaloisField;
-
-    constructor(a: number, b: number, prime: number) {
-        this.field = new PrimeGaloisField(prime);
-        this.a = new FieldElement(a, this.field);
-        this.b = new FieldElement(b, this.field);
-
-        if ( this.field.isContained(this.a) === false || this.field.isContained(this.b) === false ) {
-            throw new Error(`(${a}, ${b}) is not in fields`);
-        }
-    }
-
-    isContained(point: Point): boolean {
-        return point.y.pow(2).value == point.x.pow(3).add(this.a.mul(point.x)).add(this.b).value;
-    }
+  x: FieldElement;
+  y: FieldElement;
+  curve: EllipticCurve;
+  isIdentity(): boolean;
+  add(other: Point): Point;
+  scalarMul(scalar: number): Point;
 }
 
+class EllipticCurve implements EllipticCurveInterface {
+  a: FieldElement;
+  b: FieldElement;
+  field: PrimeGaloisField;
+
+  constructor(a: number, b: number, prime: number) {
+    this.field = new PrimeGaloisField(prime);
+    this.a = new FieldElement(a, this.field);
+    this.b = new FieldElement(b, this.field);
+
+    if (
+      this.field.isContained(this.a) === false ||
+      this.field.isContained(this.b) === false
+    ) {
+      throw new Error(`(${a}, ${b}) is not in fields`);
+    }
+  }
+
+  isContained(point: Point): boolean {
+    return (
+      point.y.pow(2).value ==
+      point.x.pow(3).add(this.a.mul(point.x)).add(this.b).value
+    );
+  }
+}
 
 class Point implements PointInterface {
-    x: FieldElement;
-    y: FieldElement;
-    curve: EllipticCurve;
+  x: FieldElement;
+  y: FieldElement;
+  curve: EllipticCurve;
 
-    constructor(x: number, y: number, curve: EllipticCurve) {
-        this.curve = curve;
-        this.x = new FieldElement(x, this.curve.field);
-        this.y = new FieldElement(y, this.curve.field);
+  constructor(x: number, y: number, curve: EllipticCurve) {
+    this.curve = curve;
+    this.x = new FieldElement(x, this.curve.field);
+    this.y = new FieldElement(y, this.curve.field);
 
-        if ( (x != 0 || y != 0) && this.curve.isContained(this) === false ) {
-            throw new Error(`(${x}, ${y}) is not on curve`);
-        }
+    if ((x != 0 || y != 0) && this.curve.isContained(this) === false) {
+      throw new Error(`(${x}, ${y}) is not on curve`);
+    }
+  }
+
+  isIdentity(): boolean {
+    return this.x.value == 0 && this.y.value == 0;
+  }
+
+  add(other: Point): Point {
+    if (this.curve != other.curve) {
+      throw new Error(`Two points are not on the same curve`);
     }
 
-    isIdentity(): boolean {
-        return this.x.value == 0 && this.y.value == 0;
+    // I + P = P
+    if (this.isIdentity()) {
+      return other;
     }
 
-    add(other: Point): Point {
-        if (this.curve != other.curve) {
-            throw new Error(`Two points are not on the same curve`);
-        }
-
-        // I + P = P
-        if (this.isIdentity()) {
-            return other;
-        }
-
-        // P + I = P
-        if (other.isIdentity()) {
-            return this;
-        }
-
-        // P + (-P) = I
-        if (this.x.value == other.x.value && this.y.value == other.y.scalarMul(-1).value) {
-            return new Point(0, 0, this.curve);
-        }
-
-        // self == other        
-        if ( this.x.value == other.x.value && this.y.value == other.y.value ) {
-            // self.y == Infinity -> I
-            if ( this.y.value == 0 ) {
-                return new Point(0, 0, this.curve);
-            }
-
-            let s: FieldElement = this.x.pow(2).scalarMul(3).add(this.curve.a).mul(this.y.scalarMul(2).inv());
-            let x3: FieldElement = s.pow(2).sub(this.x.scalarMul(2));
-            let y3: FieldElement = s.mul(this.x.sub(x3)).sub(this.y);
-
-            return new Point(x3.value, y3.value, this.curve);
-        }
-
-        // general case (this.x.value != other.x.value)
-        let s: FieldElement = other.y.sub(this.y).mul(other.x.sub(this.x).inv());
-        let x3: FieldElement = s.pow(2).sub(this.x).sub(other.x);
-        let y3: FieldElement = s.mul(this.x.sub(x3)).sub(this.y);
-
-        console.log(s, x3, y3);
-
-        return new Point(x3.value, y3.value, this.curve);
+    // P + I = P
+    if (other.isIdentity()) {
+      return this;
     }
 
-    scalarMul(scalar: number): Point {
-        if (scalar < 0) {
-            return new Point(this.x.value, this.y.scalarMul(-1).value, this.curve).scalarMul(-scalar);
-        }
-
-        let result: Point = new Point(0, 0, this.curve);
-        let base: Point = new Point(this.x.value, this.y.value, this.curve);
-
-        while (scalar > 0) {
-            // if the last bit is 1, then add
-            if ( scalar % 2 == 1 ) {
-                result = result.add(base);
-            }
-
-            scalar = Math.floor(scalar / 2);
-            base = base.add(base);
-        }
-
-        return result;
+    // P + (-P) = I
+    if (
+      this.x.value == other.x.value &&
+      this.y.value == other.y.scalarMul(-1).value
+    ) {
+      return new Point(0, 0, this.curve);
     }
+
+    // self == other
+    if (this.x.value == other.x.value && this.y.value == other.y.value) {
+      // self.y == Infinity -> I
+      if (this.y.value == 0) {
+        return new Point(0, 0, this.curve);
+      }
+
+      let s: FieldElement = this.x
+        .pow(2)
+        .scalarMul(3)
+        .add(this.curve.a)
+        .mul(this.y.scalarMul(2).inv());
+      let x3: FieldElement = s.pow(2).sub(this.x.scalarMul(2));
+      let y3: FieldElement = s.mul(this.x.sub(x3)).sub(this.y);
+
+      return new Point(x3.value, y3.value, this.curve);
+    }
+
+    // general case (this.x.value != other.x.value)
+    let s: FieldElement = other.y.sub(this.y).mul(other.x.sub(this.x).inv());
+    let x3: FieldElement = s.pow(2).sub(this.x).sub(other.x);
+    let y3: FieldElement = s.mul(this.x.sub(x3)).sub(this.y);
+
+    return new Point(x3.value, y3.value, this.curve);
+  }
+
+  scalarMul(scalar: number): Point {
+    if (scalar < 0) {
+      return new Point(
+        this.x.value,
+        this.y.scalarMul(-1).value,
+        this.curve,
+      ).scalarMul(-scalar);
+    }
+
+    let result: Point = new Point(0, 0, this.curve);
+    let base: Point = new Point(this.x.value, this.y.value, this.curve);
+
+    while (scalar > 0) {
+      // if the last bit is 1, then add
+      if (scalar % 2 == 1) {
+        result = result.add(base);
+      }
+
+      scalar = Math.floor(scalar / 2);
+      base = base.add(base);
+    }
+
+    return result;
+  }
 }
 
 export { EllipticCurve, Point };
--- src/galoisFields.ts
+++ src/galoisFields.ts
@@ -1,137 +1,139 @@
 interface PrimeGaloisFieldInterface {
-    prime: number;
-    isContained(fieldElement: FieldElement): boolean;
+  prime: number;
+  isContained(fieldElement: FieldElement): boolean;
 }
 
 interface FieldElementInterface {
-    value: number;
-    field: PrimeGaloisField;
-    valueOf(): string;
-    P(): number;
-    add(other: FieldElement): FieldElement;
-    sub(other: FieldElement): FieldElement;
-    mul(other: FieldElement): FieldElement;
-    scalarMul(n: number): FieldElement;
-    pow(exponent: number): FieldElement;
-    inv(): FieldElement;
+  value: number;
+  field: PrimeGaloisField;
+  valueOf(): string;
+  P(): number;
+  add(other: FieldElement): FieldElement;
+  sub(other: FieldElement): FieldElement;
+  mul(other: FieldElement): FieldElement;
+  scalarMul(n: number): FieldElement;
+  pow(exponent: number): FieldElement;
+  inv(): FieldElement;
 }
 
 class PrimeGaloisField implements PrimeGaloisFieldInterface {
-    prime: number;
+  prime: number;
 
-    constructor(prime: number) {
-        this.prime = prime;
-    }
+  constructor(prime: number) {
+    this.prime = prime;
+  }
 
-    isContained(fieldElement: FieldElement): boolean {
-        return 0 <= fieldElement.value && fieldElement.value < this.prime;
-    }
+  isContained(fieldElement: FieldElement): boolean {
+    return 0 <= fieldElement.value && fieldElement.value < this.prime;
+  }
 }
 
 class FieldElement implements FieldElementInterface {
-    value: number;
-    field: PrimeGaloisField;
+  value: number;
+  field: PrimeGaloisField;
 
-    constructor(value: number, field: PrimeGaloisField) {
-        this.value = value;
-        this.field = field;
+  constructor(value: number, field: PrimeGaloisField) {
+    this.value = value;
+    this.field = field;
+  }
+
+  valueOf(): string {
+    return "0x" + this.value.toString(16).padStart(64, "0");
+  }
+
+  P(): number {
+    return this.field.prime;
+  }
+
+  mod(value: number): number {
+    return value >= 0
+      ? value % this.P()
+      : ((value % this.P()) + this.P()) % this.P();
+  }
+
+  add(other: FieldElement): FieldElement {
+    if (!this.field.isContained(other)) {
+      throw new Error(`Cannot add ${other} to ${this} in ${this.field}`);
     }
 
-    valueOf(): string {
-        return "0x" + this.value.toString(16).padStart(64, "0");
+    return new FieldElement(this.mod(this.value + other.value), this.field);
+  }
+
+  sub(other: FieldElement): FieldElement {
+    if (!this.field.isContained(other)) {
+      throw new Error(`Cannot sub ${other} to ${this} in ${this.field}`);
     }
 
-    P(): number {
-        return this.field.prime;
+    return new FieldElement(this.mod(this.value - other.value), this.field);
+  }
+
+  mul(other: FieldElement): FieldElement {
+    if (!this.field.isContained(other)) {
+      throw new Error(`Cannot mul ${other} to ${this} in ${this.field}`);
     }
 
-    mod(value: number): number {
-        return value >= 0 ? value % this.P() : (value % this.P() + this.P()) % this.P();
+    return new FieldElement(this.mod(this.value * other.value), this.field);
+  }
+
+  scalarMul(n: number): FieldElement {
+    return new FieldElement(this.mod(this.value * n), this.field);
+  }
+
+  pow(exponent: number): FieldElement {
+    if (exponent < 0) {
+      return this.inv().pow(-exponent);
     }
 
-    add(other: FieldElement): FieldElement {
-        if (!this.field.isContained(other)) {
-            throw new Error(`Cannot add ${other} to ${this} in ${this.field}`);
-        }
+    let result: FieldElement = new FieldElement(1, this.field);
+    let base: FieldElement = new FieldElement(this.value, this.field);
+    while (exponent > 0) {
+      // if the last bit is 1, then multiply
+      if (exponent % 2 == 1) {
+        result = result.mul(base);
+      }
 
-        return new FieldElement(this.mod(this.value + other.value), this.field);
+      exponent = Math.floor(exponent / 2);
+      base = base.mul(base);
     }
 
-    sub(other: FieldElement): FieldElement {
-        if (!this.field.isContained(other)) {
-            throw new Error(`Cannot sub ${other} to ${this} in ${this.field}`);
-        }
+    return result;
+  }
 
-        return new FieldElement(this.mod(this.value - other.value), this.field);
-    }
+  inv(): FieldElement {
+    // extended euclidean algorithm
+    let sp: number = 1;
+    let sc: number = 0;
+    let tp: number = 0;
+    let tc: number = 1;
+    let rp: number = this.value;
+    let rc: number = this.P();
+    let q: number;
 
-    mul(other: FieldElement): FieldElement {
-        if (!this.field.isContained(other)) {
-            throw new Error(`Cannot mul ${other} to ${this} in ${this.field}`);
-        }
+    while (true) {
+      let sn: number;
+      let tn: number;
+      let rn: number;
 
-        return new FieldElement(this.mod(this.value * other.value), this.field);
-    }
+      q = Math.floor(rp / rc);
+      rn = rp % rc;
+      rp = rc;
+      rc = rn;
 
-    scalarMul(n: number): FieldElement {
-        return new FieldElement(this.mod(this.value * n), this.field);
-    }
-
-    pow(exponent: number): FieldElement {
-        if (exponent < 0) {
-            return this.inv().pow(-exponent);
-        }
-
-        let result: FieldElement = new FieldElement(1, this.field);
-        let base: FieldElement  =  new FieldElement(this.value, this.field);
-        while (exponent > 0) {
-            // if the last bit is 1, then multiply
-            if ( exponent % 2 == 1 ) {
-                result = result.mul(base);
-            }
-
-            exponent = Math.floor(exponent / 2);
-            base = base.mul(base);
-        }
-
-        return result;
-    }
-
-    inv(): FieldElement {
-        // extended euclidean algorithm
-        let sp: number = 1;
-        let sc: number = 0;
-        let tp: number = 0;
-        let tc: number = 1;
-        let rp: number = this.value;
-        let rc: number = this.P();
-        let q: number;
-
-        for (let i = 0; i < 100; i++) {
-            let sn: number;
-            let tn: number;
-            let rn: number;
-
-            q = Math.floor(rp / rc);
-            rn = rp % rc;
-            rp = rc;
-            rc = rn;
-
-            if (rn == 0) {
-                return new FieldElement(this.mod(sc), this.field);
-            }
-
-            sn = sp - q * sc;
-            sp = sc;
-            sc = sn;
-
-            tn = tp - q * tc;
-            tp = tc;
-            tc = tn;
-        }
-
+      if (rn == 0) {
         return new FieldElement(this.mod(sc), this.field);
+      }
+
+      sn = sp - q * sc;
+      sp = sc;
+      sc = sn;
+
+      tn = tp - q * tc;
+      tp = tc;
+      tc = tn;
     }
+
+    return new FieldElement(this.mod(sc), this.field);
+  }
 }
 
 export { FieldElement, PrimeGaloisField };
